c639e9262c167bd07ef6e80fa2f4aa24502f5a51
nazmiyu <thanyffak@hotmail.co.jp>
2016-05-26 17:52:30 +0900

    relation and render

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
 	def index
 		@tasks = Task.all
 	end
+
+	def new
+		@projects = Project.all
+		@task = Task.new
+	end
+
+	def create
+		@task = Task.new(task_params)
+		
+		if @task.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def task_params
+		params.require(:task).permit(:title, :project_id)
+	end
 end
